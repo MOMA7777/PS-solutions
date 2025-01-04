9ef808b87019ab72e61796157ffac189c9c81383
MOMA7777 <mazenw545@gmail.com>
2025-01-04 16:10:50 +0300

    new ONe

---
+++ 165B_Burning_Midnight_Oil.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool canGet(int n, int k, int v) {
+    int lines = 0, current = v;
+
+    while ( current > 0 ) {
+        lines += current;
+        current /= k;
+    }
+
+    return lines >= n;
+}
+
+int main() {
+    
+    int n, k;
+    cin >> n >> k;
+
+    int l = 1, r = n;
+    while (l < r) {
+        int m = l + (r - l) / 2;
+
+        if ( canGet(n, k, m) ) {
+            r = m;
+        }
+        else 
+            l = m + 1;
+    }
+
+    cout << l << endl;
+
+    return 0;
+}
+
